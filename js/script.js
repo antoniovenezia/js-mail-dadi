8a2ac106b1132910f7b6487f098db8852606f364
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-01 00:20:08 +0200

    js-mail-dadi

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// PRIMA PARTE DELL'ESERCIZIO - MAIL
+var userMail = prompt("Qual'è il tuo indirizzo email?");
+console.log("mail");
+
+var allEmail = ["ciao@gmail.com", "salve@hotmail.it", "buongiorno@alice.it", "arrivederci@libero.it"];
+
+var check = false;
+
+for (var i = 0; i < allEmail.length; i++) {
+    if (allEmail[i] === "salve@hotmail.it") {
+        check = true;
+        break;
+    }
+}
+
+if (check) {
+    document.getElementById("mail").innerHTML = "Ok, puoi accedere al portale!";
+    } else {
+        document.getElementById("mail").innerHTML = "Ci dispiace, non puoi accedere al portale!";
+    }
+
+
+
+// SECONDA PARTE DELL'ESERCIZIO - DADI
+var x = Math.round(Math.random()*10);
+var y = Math.round(Math.random()*10);
+
+
+if (x > y) {
+    document.getElementById("numbers").innerHTML += x + " Numero random player";
+} else {
+    document.getElementById("numbers").innerHTML += y + " Numero random computer";
+}
